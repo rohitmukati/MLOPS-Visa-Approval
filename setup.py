5ade32ea7468ee92d4080fae70e1021fad72c8b8
mukatirohit <rohanmukati2002@gmail.com>
2025-06-11 15:18:20 +0530

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Rohit",
+    author_email="rohanmukati2002@gmail.com",
+    packages=find_packages()
+)
